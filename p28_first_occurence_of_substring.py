31a20d7244041badc32bc475833cde2d11879145
Ebad-ur-Rehman <ebadinfalltraders@gmail.com>
2024-07-18 21:40:33 +0500

    p28_first_occurence_of_substring. added

--- p28_first_occurence_of_substring.py
+++ p28_first_occurence_of_substring.py
@@ -1,14 +1,16 @@
-haystack = "hello"
-needle = "ll"
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        needle_length = len(needle)
+        starting_letter = needle[0]
 
-needle_length = len(needle)
-starting_letter = needle[0]
-
-print(haystack[2:needle_length+2])
-
-for i, char in enumerate(haystack):
-    if char == starting_letter and haystack[i:needle_length] == needle:
-        print(i)
-
-print(-1)
+        for i, char in enumerate(haystack):
+            if char == starting_letter and haystack[i:needle_length+i] == needle:
+                return i
+        
+        return -1
 
